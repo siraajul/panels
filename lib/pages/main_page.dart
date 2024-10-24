b5b21955f941f7eb98699c1c92fa3a523c7c8d2c
siraajul <shirajulislamparvez@gmail.com>
2024-10-25 00:51:12 +0600

    Add -> BottomNavigationBar with Set State

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:panels/pages/account_page.dart';
+import 'package:panels/pages/explore_page.dart';
+import 'package:panels/pages/home_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,12 +11,40 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+  List<Widget> _pages = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pages = [
+      const HomePage(),
+      const ExplorePage(),
+      const AccountPage(),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('MainPage'),
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Explore'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_box_outlined), label: 'Account')
+          ]),
     );
   }
 }
