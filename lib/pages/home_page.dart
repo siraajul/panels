840af357cb3a11126e3a159cc958e9960117d5c9
siraajul <shirajulislamparvez@gmail.com>
2024-10-25 14:43:32 +0600

    Update: --> MasonryGridView Properties

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:panels/widgets/tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,9 +12,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomePage'),
+    return Scaffold(
+      body: MasonryGridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 12,
+        crossAxisSpacing: 12,
+        padding: const EdgeInsets.all(8),
+        itemBuilder: (BuildContext context, int index) {
+          return ImageTile(
+            index: index,
+            extent: (index % 2) == 0 ? 300 : 150,
+            imageSource: 'https://picsum.photos/500/500?random=$index',
+          );
+        },
       ),
     );
   }
