54eef30c0e189e8fce081288f58c8ce2eae3b9ea
siraajul <shirajulislamparvez@gmail.com>
2024-10-26 19:25:56 +0600

    Add:--> AfterScroll Result

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:panels/widgets/tile.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  final Function(bool) afterScrollResult;
+
+  const HomePage({super.key, required this.afterScrollResult});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _isVisible = true;
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        //Result : false
+        if (_isVisible) {
+          _isVisible = false;
+          widget.afterScrollResult(_isVisible);
+        }
+      }
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.forward) {
+        //Result : true
+        if (!_isVisible) {
+          _isVisible = true;
+          widget.afterScrollResult(_isVisible);
+        }
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: DefaultTabController(
         length: 3,
         child: NestedScrollView(
+          controller: _scrollController,
           headerSliverBuilder: (context, innerBoxIsScrolled) {
             return [
               SliverAppBar(
@@ -26,7 +56,8 @@ class _HomePageState extends State<HomePage> {
                     height: 50,
                     width: 50,
                     decoration: const BoxDecoration(
-                        color: Colors.grey, shape: BoxShape.circle),
+                        color: Colors.red, shape: BoxShape.circle),
+                    child: Icon(Icons.person_2_rounded,size: 35,color: Colors.white,),
                   ),
                 ),
                 bottom: const TabBar(
